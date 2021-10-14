1e4439f1441e5c26b4d6ae963031973ee61af00d
DutchMon <dutch.holland13@gmail.com>
2021-10-14 00:01:38 -0700

    change PROD_URL in config

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://capstone-project-xi.vercel.app';
+export const server = dev ? 'http://localhost:3000' : 'capstone-project-xi.vercel.app';
