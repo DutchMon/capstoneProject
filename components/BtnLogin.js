dfd08485b51d73748a448f48a2c48f9f19cb0c57
DutchMon <dutch.holland13@gmail.com>
2021-11-03 11:36:13 -0700

    debug

--- components/BtnLogin.js
+++ components/BtnLogin.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { signIn } from 'next-auth/react'
-
+import { useRouter } from 'next/router'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebook, faGithub, faGoogle, faTwitter } from '@fortawesome/free-brands-svg-icons'
